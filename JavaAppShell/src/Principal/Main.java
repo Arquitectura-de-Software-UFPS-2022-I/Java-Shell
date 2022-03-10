ff0aadaa62e668abb9e44ee24369f9b497bfb8b2
LinaVera <gelinava@gmail.com>
2022-03-09 19:44:48 -0500

    Firmar PDF

--- JavaAppShell/src/Principal/Main.java
+++ JavaAppShell/src/Principal/Main.java
@@ -1,16 +1,16 @@
 package Principal;
 
-import Control.Controller;
-import Model.Pdf;
-import Model.User;
-import Services.Conexion;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
+import models.FileDto;
+import models.SignatureRequestDto;
+import models.SignatureRequestUserDto;
+import models.UserDto;
+import services.impl.*;
+
 /**
  *
  * @author Java-Shell
@@ -18,178 +18,262 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
-    Conexion c = new Conexion();
-    static Controller controller = new Controller();
-    static User user = new User();
+
+    static UserServiceImpl us = new UserServiceImpl();
+    static UserDto udto = new UserDto();
+
+    static FileServiceImpl fs = new FileServiceImpl();
+    static FileDto fdto = new FileDto();
+
+    static SignatureRequestDto stDto = new SignatureRequestDto();
+    static SignatureRequestServiceImpl srs = new SignatureRequestServiceImpl();
+
+    static SignatureRequestUserDto srudto = new SignatureRequestUserDto();
+    static SignatureRequestUserServiceImpl srus = new SignatureRequestUserServiceImpl();
+
     static int options = 1;
     static int option = 1;
 
     //Start menu
-    //1. Iniciar sesión 
+    //1. Iniciar sesión - R2. Inicio de sesion
     public static boolean logIn() {
-        boolean res = true;
-        System.out.println("\nDigite código");
-        int cod = sc.nextInt();
-        System.out.println("Digite su contraseña");
-        String pass = sc.next();
-        if (cod != 0 && pass != "") {
-            //call controller
-            res = controller.login(cod, pass);
+        try {
+            System.out.println("\nDigite nombre de usuario");
+            String username = sc.next();
+            System.out.println("Digite su contraseña");
+            String pass = sc.next();
+            //Llamo al UserServiceImpl
+            udto = us.loginUser(pass, username);
+            System.out.println(udto);
+            return (udto != null);
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            return false;
         }
-        return res;
     }
 
-    //2. Regitrar nuevo usuario
+    //2. Regitrar nuevo usuario - R1. Registro de usuario
     public static void singUp() {
-        System.out.println("--Registrar--");
-        System.out.println("\nDigite código");
-        int cod = sc.nextInt();
-        user.setId_user(cod);
-        System.out.println("Digite su nombre");
-        String name = sc.next();
-        user.setName(name);
-        System.out.println("Digite su email");
-        String mail = sc.next();
-        user.setEmail(mail);
-        System.out.println("Digite su contraseña");
-        String pass = sc.next();
-        user.setPassword(pass);
-        if (cod != 0 && name != "" && mail != "" && pass != "") {
-            //call controller
-            controller.singUp(user);
-        } else {
-            System.out.println("Fallo al registrar");
+        try {
+            System.out.println("--Registrar--");
+            System.out.println("Digite su nombre");
+            String name = sc.next();
+            udto.setFull_name(name);
+            System.out.println("Digite su nombre de usuario");
+            String username = sc.next();
+            udto.setUsername(username);
+
+            System.out.println("Digite su email");
+            String mail = sc.next();
+            udto.setEmail(mail);
+            System.out.println("Digite su contraseña");
+            String pass = sc.next();
+            udto.setPassword(pass);
+            System.out.println(us.saveUser(udto));
+
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
         }
     }
 
     //Services menu
-    //1. Subir firma
-    public static void uploadSignature() {
-    }
-
-    //2. Subir PDF
-    public static void uploadPDF(String file) {
-
-        Pdf pa = new Pdf();
-        File f;
-        f = new File(file);
-        System.out.println("Digite la coordenada X");
-        float x = sc.nextFloat();
-        pa.setPos_x(x);
-        System.out.println("Digite la coordenada Y");
-        float y = sc.nextFloat();
-        pa.setPos_y(y);
-        pa.setStatus(0);
+    //1. Subir firma - R3. Subir firmas
+    public static List uploadSignature() {
         try {
-            byte[] pdf = new byte[(int) f.length()];
-            InputStream input = new FileInputStream(f);
-            input.read(pdf);
-            pa.setFile(pdf);
-            System.out.println("siii");
+            System.out.println("Digite la ruta de la firma a registrar");
+            String path = sc.next();
+            path = path.replace("\\", "/");
+            String[] parts = path.split("/");
+            String nameIMG = parts[parts.length - 1];
+            System.out.println(nameIMG);
+            File file = new File(path);
+
+            System.out.println(fs.saveFile(nameIMG, file));
+            List<FileDto> lista = fs.getFiles();
+            return lista;
+
         } catch (Exception e) {
-            pa.setFile(null);
-            System.out.println("noo");
+            System.out.println("Error" + e.getMessage());
+
+            return null;
+
         }
-        controller.uploadPDF(pa);
+
     }
 
-    //3. Solicitar firmas 
+    //2. Subir PDF - R5. Subir documento
+    public static List uploadPDF() {
+        try {
+            System.out.println("Digite la ruta del PDF a guardar");
+            String path = sc.next();
+            path = path.replace("\\", "/");
+            String[] parts = path.split("/");
+            String namePDF = parts[parts.length - 1];
+
+            File file = new File(path);
+
+            System.out.println(fs.saveFile(namePDF, file));
+            List<FileDto> lista = fs.getFiles();
+            return lista;
+
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+
+            return null;
+
+        }
+    }
+
+    //3. Solicitar firmas - R7.	Solicitud de firma
     public static void requestSignature() {
-//        String em = "linavanessavepu@ufps.edu.co";
-//        String pass = "linaca01";
-//
-//        SimpleEmail email = new SimpleEmail();
-//
-//        email.setHostName("smtp.gmail.com");
-//        email.setSmtpPort(465);
-//        email.setAuthenticator(new DefaultAuthenticator(em, pass));
-//        email.setSSLOnConnect(true);
-//
-//        try {
-//            email.setFrom(em);
-//            email.setSubject("prueba");
-//            email.setMsg("Enviando email desde java");
-//            email.addTo(em);
-//            email.send();
-//            System.out.println("Envio exitoso");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            String date = dtf.format(LocalDateTime.now());
+            stDto.setCreate_date(date);
+
+            System.out.println("Digite el asunto de la solicitud");
+            String subject = sc.next();
+            stDto.setSubject(subject);
+
+            System.out.println("Digite el ID del documento");
+            int document_id = sc.nextInt();
+            stDto.setDocument(document_id);
+
+            System.out.println("Digite posicion X del documento");
+            int x = sc.nextInt();
+            srudto.setPos_x(x);
+            System.out.println("Digite posicion Y del documento");
+            int y = sc.nextInt();
+            srudto.setPos_y(y);
+            System.out.println("Digite el numero de página del documento a firmar");
+            int num_page = sc.nextInt();
+            srudto.setNum_page(num_page);
+            srudto.setSigned(false);
+            srudto.setCreated_date(date);
+
+            System.out.println("Digite del usuario a enviar solicitud");
+            int user_id = sc.nextInt();
+            stDto.setUser(user_id);
+
+            srudto.setUser(user_id);
+
+            stDto = srs.saveRequest(stDto);
+            srudto.setRequest(stDto.getId());
+
+            srudto = srus.saveRequest(srudto);
+
+            System.out.println(stDto);
+            System.out.println(srudto);
+
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
     //4. Mostrar todos los PDFs 
     public static void showPDF() {
-    }
-    //read document
 
-    //5. Firmar PDF
+    }
+
+//4	Verificación de firma
+//6	Listado de documentos
+//10	Listado de documentos pendientes por firmar
+//11	Listado de solicitudes de firmas aprobadas
+//12	Listado de solicitudes de firmas pendientes
+//13	Listado historico de firmas
+    //5. Firmar PDF - R8	Firmar documento
     public static void signPDF() {
-//        try {
-//            // path where the pdf is to be created.
-//            String path = "C:\\Users\\Lina\\OneDrive\\Escritorio\\Calendario2022-1.pdf";
-//            PdfWriter pdfwriter = new PdfWriter(path);
-//
-//            //Creating a PdfDocument object.
-//            //passing PdfWriter object constructor
-//            PdfDocument pdfdocument
-//                    = new PdfDocument(pdfwriter);
-//
-//            // Creating a Document and
-//            // passing pdfDocument object
-//            Document document = new Document(pdfdocument);
-//
-//            // Create an ImageData object
-//            String imageFile = "C:\\Users\\Lina\\OneDrive\\Imágenes\\ink.png";
-//            ImageData data
-//                    = ImageDataFactory.create(imageFile);
-//            // Creating an Image object
-//            Image image = new Image(data);
-//
-//            // Set the position of the image.
-//            image.setFixedPosition(50, 100);
-//            image.setWidth(64);
-//            image.setHeight(64);
-//
-//            // Adding image to the document
-//            document.add(image);
-//            // Closing the document
-//            document.close();
-//
-//            System.out.println(
-//                    "Image  position set successfully in pdf");
-//        } catch (Exception e) {
-//            System.out.println(
-//                    "unable to set image position due to " + e);
-//        }
+        try {
+            //pedir la request y y aprobar la firma""
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            String date = dtf.format(LocalDateTime.now());
+            System.out.println("Digite el ID de la solicitud que desea firmar");
+            int request_id = sc.nextInt();
+            srudto = srus.getRequestId(request_id);
+            srudto.setSigned(true);
+            srudto.setSignature_date(date);
+            srus.updateRequest(srudto);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
-    //6. Descargar PDF
+    //6. Descargar PDF - R9. Generar PDF firmado
     public static void downloadPDF() {
-    }
-/////////////////////////////////////////////////////////////////////
-    public static void archivo() {
         try {
-            Scanner scs = new Scanner(System.in);
-            String ruta;
-            System.out.println("Introduzca la ruta del fichero que desea leer");
-            ruta = scs.nextLine();
-            ruta = ruta.replace("\\", "\\\\");
-            System.out.println(ruta);
-            BufferedReader br = new BufferedReader(new FileReader(ruta));
-            String linea = "";
-            while (linea != null) {
-                linea = br.readLine();
-                if (linea != null) {
-                    System.out.println(linea);
-                }
-            }
-        } catch (IOException ex) {
-            System.out.println("No se ha encontrado el archivo." + ex);
+            System.out.println("Digite el codigo del PDF a descargar");
+            int cod = sc.nextInt();
+            System.out.println("Digite dirección");
+            String dir = sc.next();
+            fs.downloadFileSigned(dir, cod);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void showOptions() {
+        //Services menu
+        System.out.println("************Servicios************* \n"
+                + "    1. Subir firma \n"
+                + "    2. Subir PDF \n"
+                + "    3. Solicitar firmas \n"
+                + "    4. Mostrar todos los PDFs \n"
+                + "    5. Firmar PDF \n"
+                + "    6. Descargar PDF \n"
+                + "    0. Cerrar sesión \n"
+                + "********************************");
+    }
 
+    public static void options() {
+        while (option != 0) {
+            showOptions();
+            System.out.println("Ingresa el número del servicio");
+            option = sc.nextInt();
+            switch (option) {
+                case 0:
+                    //End
+                    option = 0;
+                    System.out.println("Sesión cerrada");
+                    break;
+                case 1:
+                    //Upload signature
+                    System.out.println("Subir firma");
+                    uploadSignature();
+                    break;
+                case 2:
+                    //Upload PDF
+                    System.out.println("Subir PDF");
+                    uploadPDF();
+                    break;
+                case 3:
+                    //Request signature
+                    System.out.println("Solicitar firmas");
+                    requestSignature();
+                    break;
+                case 4:
+                    //Show PDFs
+                    System.out.println("Mostrar PDFs");
+                    showPDF();
+                    break;
+                case 5:
+                    //Sing a PDF
+                    System.out.println("Firmar PDF");
+                    signPDF();
+                    break;
+                case 6:
+                    //Download a PDF
+                    System.out.println("Descargar PDFs");
+                    downloadPDF();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    /////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
         try {
             //Start menu
             while (options != 0) {
@@ -204,68 +288,11 @@ public class Main {
                 if (options == 1) {
                     //Log in
                     if (logIn()) {
-                        option = 1;
                         System.out.println("---Bienvenido---");
-                        while (option != 0) {
-                            //Services menu
-                            System.out.println("************Servicios************* \n"
-                                    + "    1. Subir firma \n"
-                                    + "    2. Subir PDF \n"
-                                    + "    3. Solicitar firmas \n"
-                                    + "    4. Mostrar todos los PDFs \n"
-                                    + "    5. Firmar PDF \n"
-                                    + "    6. Descargar PDF \n"
-                                    + "    0. Cerrar sesión \n"
-                                    + "********************************");
-                            System.out.println("Ingresa el número del servicio");
-                            option = sc.nextInt();
-                            switch (option) {
-                                case 0:
-                                    //End
-                                    controller.logOut();
-                                    option = 0;
-                                    System.out.println("Sesión cerrada");
-                                    break;
-                                case 1:
-                                    //Upload signature
-                                    System.out.println("Subir firma");
-                                    archivo();
-                                    break;
-                                case 2:
-                                    //Upload PDF
-                                    System.out.println("Subir PDF");
-                                    String file;
-                                    System.out.println("Introduzca la ruta del fichero que desea leer");
-                                    String saltoDeLinea = sc.nextLine();
-                                    file = sc.nextLine();
-                                    uploadPDF(file);
-                                    break;
-                                case 3:
-                                    //Request signature
-
-                                    System.out.println("Solicitar firmas");
-                                    break;
-                                case 4:
-                                    //Show PDFs
-                                    System.out.println("Mostrar PDFs");
-                                    break;
-                                case 5:
-                                    //Sing a PDF
-                                    System.out.println("Firmar PDF");
-                                    signPDF();
-                                    break;
-                                case 6:
-                                    //Download a PDF
-                                    System.out.println("Descargar PDFs");
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
+                        options();
                     } else {
-                        System.out.println("Fallo al iniciar sesión");
+                        System.out.println("Usuario no registrado");
                     }
-
                 } else if (options == 2) {
                     //Sing up
                     singUp();
